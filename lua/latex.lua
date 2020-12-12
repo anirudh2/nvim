35fb233364c65234fe66babd682de827f1595595
Anirudh Patel <19479805+anirudh2@users.noreply.github.com>
2020-12-11 22:47:41 -0700

    Proper credit for code source

--- lua/latex.lua
+++ lua/latex.lua
@@ -1,6 +1,8 @@
-require"completion".addCompletionSource("vimtex", require"vimtex".complete_item)
+-- From https://github.com/nvim-lua/completion-nvim/wiki/custom-completion-source#create-completion-source-with-omni-func
 
 -- Completion Chain
+require"completion".addCompletionSource("vimtex", require"vimtex".complete_item)
+
 vim.api.nvim_command( "let g:completion_chain_complete_list = { 'tex' : [{'complete_items': ['vimtex', 'lsp']}] }" )
 
 -- Auto switch completion sources
