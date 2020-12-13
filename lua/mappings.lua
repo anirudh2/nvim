196621c12b6e40547dc664c74a6efed8e8346d51
Anirudh Patel <19479805+anirudh2@users.noreply.github.com>
2020-12-12 21:35:12 -0700

    More mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,12 +9,25 @@ local mappings = {
   ["ikk"] = { "<Esc>", noremap = true; };
 
   -- Quickjump
-  ["n<leader><leader>"] = { "/<++><Enter>:noh<Enter>cf>" };
+  ["n<leader><leader>"] = { "/<++><Enter>:noh<CR>cf>" };  -- Leader Leader -> Find and replace next <++>
+
+  -- Remove Highlighting
+  ["n<leader>nh"] = { ":noh<CR>" };  -- Leader nh -> remove highlighting
 
   -- Navigating autocomplete
   ["i<Space>"] = { 'pumvisible() ? "\\<c-y>" : "\\<Space>"', expr=true; noremap=false; silent=false };  -- Space -> Enter
   ["i<S-Tab>"] = { 'pumvisible() ? "\\<c-p>" : "\\<S-Tab>"', expr=true; noremap=false; silent=false };  -- Shift-tab -> Up
   ["i<Tab>"] = { 'pumvisible() ? "\\<c-n>" : "\\<Tab>"', expr=true; noremap=false; silent=false };  -- Tab -> Down
+
+  -- Tagbar
+  ["n<leader>tb"] = { ":TagbarToggle<CR>" };  -- Leader tb -> TagbarToggle
+
+  -- Wordy
+  ["n<leader>nw"] = { ":NextWordy<CR>" };  -- Leader nw -> NextWordy
+  ["n<leader>pw"] = { ":PrevWordy<CR>" };  -- Leader pw -> PrevWordy
+
+  -- NERDTree
+  ["n<leader>nt"] = { ":NERDTreeToggle<CR>" };  -- Leader nt -> Toggle NERDTree
 }
 
 nvim_mappings(mappings, { silent = true; })
